c3a7e8518740bc65f80b511492ed5a7d28c97655
chromicle <ajayprabhakar369@gmail.com>
2020-05-28 00:33:43 +0530

    add fluter protfolio

---
+++ portfolio/lib/main.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Portfolio | Ajay Prabhakar',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Ajay Prabhakar\'s portfolio'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kAppPrimaryColor,
+      body: Stack(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(25),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      AppBarButton(
+                        icon: Icons.arrow_back,
+                      ),
+                      AppBarButton(icon: Icons.menu,),
+                    ],
+                  ),
+                ),
+                AvatarImage(),
+                SizedBox(
+                  height: 30,
+                ),
+                SocialIcons(),
+                SizedBox(height: 30),
+                Text(
+                  'chromicle',
+                  style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: "Poppins"),
+                ),
+                Text(
+                  '@amFOSS',
+                  style: TextStyle(fontWeight: FontWeight.w300),
+                ),
+                SizedBox(height: 15),
+                Text(
+                  'Mobile App Developer and Open source enthusiastic',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontFamily: "Poppins"),
+                ),
+                SizedBox(height: 100,),
+                Text(
+                  'Built with < flutter />',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 15, fontFamily: "Poppins",color: Colors.blue),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppBarButton extends StatelessWidget {
+  final IconData icon;
+
+  const AppBarButton({this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 55,
+      height: 55,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: kAppPrimaryColor,
+          boxShadow: [
+            BoxShadow(
+              color: kLightBlack,
+              offset: Offset(1, 1),
+              blurRadius: 10,
+            ),
+            BoxShadow(
+              color: kWhite,
+              offset: Offset(-1, -1),
+              blurRadius: 10,
+            ),
+          ]),
+      child: Icon(
+        icon,
+        color: fCL,
+      ),
+    );
+  }
+}
+
+class AvatarImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 150,
+      height: 150,
+      padding: EdgeInsets.all(8),
+      decoration: avatarDecoration,
+      child: Container(
+        decoration: avatarDecoration,
+        padding: EdgeInsets.all(3),
+        child: Container(
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/images/user.png'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SocialIcons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SocialIcon(
+          color: Color(0xFFd44638),
+          iconData: FlutterIcons.mail_ent,
+          onPressed: () {
+            _launchURL("mailto: ajayprabhakar369@gmail.com");
+          },
+        ),
+        SocialIcon(
+          color: Color(0xFF211F1F),
+          iconData: FlutterIcons.github_ant,
+          onPressed: () {
+            _launchURL("https://github.com/chromicle");
+          },
+        ),
+        SocialIcon(
+          color: Color(0xFF38A1F3),
+          iconData: twitter,
+          onPressed: () {
+            _launchURL("https://twitter.com/chromicle_3");
+          },
+        ),
+        SocialIcon(
+          color: Color(0xFF0088CC),
+          iconData: FlutterIcons.telegram_plane_faw5d,
+          onPressed: () {
+            _launchURL("https://t.me/chromicle");
+          },
+        )
+      ],
+    );
+  }
+
+  _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+}
+
+class SocialIcon extends StatelessWidget {
+  final Color color;
+  final IconData iconData;
+  final Function onPressed;
+
+  SocialIcon({this.color, this.iconData, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Padding(
+      padding: EdgeInsets.only(left: 20.0),
+      child: Container(
+        width: 45.0,
+        height: 45.0,
+        decoration: BoxDecoration(shape: BoxShape.circle, color: color),
+        child: RawMaterialButton(
+          shape: CircleBorder(),
+          onPressed: onPressed,
+          child: Icon(iconData, color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
