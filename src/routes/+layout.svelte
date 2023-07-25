e484df2224a4ac07a3f8f653e4d4cf80b4128b7d
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-07-26 00:14:22 +0530

    [feat] add navigation bar and navigations

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { getStores } from "$app/stores"
+	import { updateFragmentLinkTarget } from "$lib/link"
+	import { onMount } from "svelte"
+  import Nav from "../components/Nav.svelte";
+
+	const { page } = getStores()
+	let element: HTMLElement
+
+	onMount(() => {
+		page.subscribe(() => {
+			if (element) {
+				// Rewrite <a> elements with a # to respect <base href="/">
+				updateFragmentLinkTarget(window.location.href, element)
+			}
+		})
+
+		navigator.serviceWorker
+			.getRegistration()
+			.then((registration) => registration?.unregister())
+	})
+</script>
+
+<svelte:window
+	on:hashchange={() => updateFragmentLinkTarget(window.location.href, element)}
+/>
+
+<div class="container" bind:this={element}>
+	<Nav />
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.container {
+		max-width: 60em;
+		margin: 0 auto;
+	}
+	main {
+		position: relative;
+	}
+</style>
