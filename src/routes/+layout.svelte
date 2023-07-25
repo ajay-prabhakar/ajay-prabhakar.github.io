4a099ac0f4c52b80a02a22ee57d82dea82e2ce91
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-07-26 00:38:55 +0530

    [feat] add tailwind

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,44 @@
 <script lang="ts">
-	import { getStores } from "$app/stores"
-	import { updateFragmentLinkTarget } from "$lib/link"
-	import { onMount } from "svelte"
+  import { getStores } from "$app/stores";
+  import { updateFragmentLinkTarget } from "$lib/link";
+  import { onMount } from "svelte";
   import Nav from "../components/Nav.svelte";
+  import "../app.css";
 
-	const { page } = getStores()
-	let element: HTMLElement
+  const { page } = getStores();
+  let element: HTMLElement;
 
-	onMount(() => {
-		page.subscribe(() => {
-			if (element) {
-				// Rewrite <a> elements with a # to respect <base href="/">
-				updateFragmentLinkTarget(window.location.href, element)
-			}
-		})
+  onMount(() => {
+    page.subscribe(() => {
+      if (element) {
+        // Rewrite <a> elements with a # to respect <base href="/">
+        updateFragmentLinkTarget(window.location.href, element);
+      }
+    });
 
-		navigator.serviceWorker
-			.getRegistration()
-			.then((registration) => registration?.unregister())
-	})
+    navigator.serviceWorker
+      .getRegistration()
+      .then((registration) => registration?.unregister());
+  });
 </script>
 
 <svelte:window
-	on:hashchange={() => updateFragmentLinkTarget(window.location.href, element)}
+  on:hashchange={() => updateFragmentLinkTarget(window.location.href, element)}
 />
 
 <div class="container" bind:this={element}>
-	<Nav />
-	<main>
-		<slot />
-	</main>
+  <Nav />
+  <main>
+    <slot />
+  </main>
 </div>
 
 <style>
-	.container {
-		max-width: 60em;
-		margin: 0 auto;
-	}
-	main {
-		position: relative;
-	}
+  .container {
+    max-width: 60em;
+    margin: 0 auto;
+  }
+  main {
+    position: relative;
+  }
 </style>
