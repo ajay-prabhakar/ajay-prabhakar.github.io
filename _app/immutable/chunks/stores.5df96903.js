2fbb82ba70ebb2f6233497e5586fa1c9de704d78
ajay-prabhakar <ajayprabhakar369@gmail.com>
2023-07-28 10:27:46 +0530

    Updates

---
+++ _app/immutable/chunks/stores.5df96903.js
@@ -0,0 +1,1 @@
+import{d as r}from"./singletons.b63f9ac6.js";const e=()=>{const s=r;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},n={subscribe(s){return e().page.subscribe(s)}},i={subscribe(s){return e().navigating.subscribe(s)}};export{e as g,i as n,n as p};
